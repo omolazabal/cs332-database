58e1c6d83b966d8ba578471759a6ed5403b55a01
Oscar Olazabal <oscarolazabal@gmail.com>
2018-11-30 15:02:28 -0800

    add error checking

--- results.php
+++ results.php
@@ -1,19 +1,23 @@
 <html>
 <body>
 <?php
-    $lines = file('config');
+    $lines = explode("\n", file_get_contents('config'));
     $link = mysql_connect($lines[0], $lines[1], $lines[2]);
     if (!$link) {
         die('Could not connect: ' . mysql_error());
     }
     echo 'Connected successfully<p>';
-    mysql_select_db($username, $link);
+    mysql_select_db($lines[1], $link);
 
     $query = "SELECT * FROM professor WHERE ssn=" . $_POST["sno"];
-    if (!$sql) {
+    if (!$query) {
         die('Error: ' . mysql_error());
     }
+
     $result = mysql_query($query,$link);
+    if (!$result) {
+        die('Error: ' . mysql_error());
+    }
 
     printf("SSN: %s<br>\n", mysql_result($result, 0, "ssn"));
     printf("First NAME: %s<br>\n", mysql_result($result, 0, "name"));
